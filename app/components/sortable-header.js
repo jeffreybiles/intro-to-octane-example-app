87a5f95b5cdde10de8cd7ca1fc65947082304d1f
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-25 14:49:18 -0700

    Highlighting of selected header

--- app/components/sortable-header.js
+++ app/components/sortable-header.js
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class SortableHeaderComponent extends Component {
+  get isSort(){
+    return this.args.sortBy && this.args.sortBy === this.args.sortedBy;
+  }
 }
