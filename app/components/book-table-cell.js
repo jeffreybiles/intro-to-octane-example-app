2a0049660753cd80036c5b252e55a3e72b926c64
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-25 12:22:33 -0700

    attempt at improving via BookTableCell Component

---
+++ app/components/book-table-cell.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+
+export default class BookTableCellComponent extends Component {
+  get isSelected(){
+    return this.args.selectedBookIds.includes(this.args.book.id);
+  }
+
+  get isStriped() {
+    return this.args.index % 2 === 0
+  }
+}
