a69621b943caae65a4a273bffa5a700af6364ffe
jeffreybiles <bilesjeffrey@gmail.com>
2019-03-27 21:49:06 -0500

    put in covers, improved /book show page

--- app/routes/books.js
+++ app/routes/books.js
@@ -8,60 +8,70 @@ export default class BooksRoute extends Route {
       author: 'Aldous Huxley',
       publicationYear: 1932,
       pages: 248,
+      imgUrl: '/imgs/brave-new-world-cover.jpg'
     }, {
       id: 2,
       name: 'Way of Kings',
       author: 'Brandon Sanderson',
       publicationYear: 2010,
       pages: 1007,
+      imgUrl: '/imgs/way-of-kings.jpg'
     }, {
       id: 3,
       name: 'Gulag Archipelago',
       author: 'Aleksandr Solzhenitsyn',
       publicationYear: 1973,
       pages: 2032,
+      imgUrl: '/imgs/gulag-archipelago.jpg'
     }, {
       id: 4,
       name: 'Fire Upon the Deep',
       author: 'Vernor Vinge',
       publicationYear: 1992,
       pages: 391,
+      imgUrl: '/imgs/fire-upon-the-deep.jpg'
     }, {
       id: 5,
       name: 'Speaker for the Dead',
       author: 'Orson Scott Card',
       publicationYear: 1986,
-      pages: 415
+      pages: 415,
+      imgUrl: '/imgs/speaker-for-the-dead.jpg'
     }, {
       id: 6,
       name: 'My Hero Academia volume 1',
       author: 'Kohei Horikoshi',
       publicationYear: 2014,
-      pages: 192
+      pages: 192,
+      imgUrl: '/imgs/boku-no-hero-academia-vol-1.jpg'
     }, {
       id: 7,
       name: 'Eye of the World',
       author: 'Robert Jordan',
       publicationYear: 1990,
-      pages: 685
+      pages: 685,
+      imgUrl: '/imgs/eye-of-the-world.jpg'
     }, {
       id: 8,
       name: 'Dune',
       author: 'Frank Herbert',
       publicationYear: 1965,
-      pages: 412
+      pages: 412,
+      imgUrl: '/imgs/dune.jpg'
     }, {
       id: 9,
       name: 'Anathem',
       author: 'Neal Stephenson',
       publicationYear: 2008,
-      pages: 937
+      pages: 937,
+      imgUrl: '/imgs/anathem.jpg'
     }, {
       id: 10,
       name: 'Wrinkle in Time',
       author: 'Madeleine L\'Engle',
       publicationYear: 1962,
-      pages: 228
+      pages: 228,
+      imgUrl: '/imgs/wrinkle-in-time.jpg'
     }]
   }
 }
